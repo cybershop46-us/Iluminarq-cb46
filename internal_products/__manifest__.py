fee6e7e94c4151ba61e3a9f64521e8e193b1fe05
cybershop46-us <cybershop46@hotmail.com>
2025-03-06 15:04:01 -0600

    updaet
    update

--- internal_products/__manifest__.py
+++ internal_products/__manifest__.py
@@ -11,7 +11,7 @@
     """,
 
     'author': "CB 46",
-    'website': "https://itecnologiagt.com",
+    'website': "https://cybershop46.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
